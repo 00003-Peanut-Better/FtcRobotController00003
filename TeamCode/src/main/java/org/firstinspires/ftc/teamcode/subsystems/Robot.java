79cc2f26a0e0c1a8dc72622dac5298b425baa506
Henwyyyy <127368464+skushy91@users.noreply.github.com>
2026-02-25 16:30:37 +1000

    basic mecanum drive base with 2 drivers

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Robot.java
@@ -0,0 +1,76 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode.subsystems;
+import static java.lang.Math.abs;
+
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Robot {
+    public Drive driveBase;
+    Gamepad gamepad1;
+    Gamepad gamepad2;
+    //public Slide slides;
+
+    public Robot(HardwareMap map, Gamepad gamepad1, Gamepad gamepad2) {
+        driveBase = new Drive(map);
+        this.gamepad1 = gamepad1;
+        this.gamepad2 = gamepad2;
+    }
+
+    public void driveWithGamePad1(){
+        if (abs(gamepad2.left_stick_x + gamepad2.left_stick_y + gamepad2.right_stick_x) < 0.1) {
+            double forward = gamepad1.left_stick_y;
+            double strafe = gamepad1.left_stick_x;
+            double turn = gamepad1.right_stick_x;
+
+            boolean controlSpeed = gamepad1.x;
+
+            if (controlSpeed) {
+                driveBase.drive(forward * 0.3, strafe * 0.3, turn * 0.3);
+            } else {
+                driveBase.drive(forward*0.7, strafe*0.7, turn*0.7);
+            }
+        }
+    }
+
+    public void driveWithGamePad2() {
+        double forward = gamepad2.left_stick_y;
+        double strafe = gamepad2.left_stick_x;
+        double turn = gamepad2.right_stick_x;
+
+        if(abs(forward) > 0.1 || abs(strafe) > 0.1 || abs(turn) > 0.1){
+            driveBase.drive(forward*0.3, strafe*0.3, turn*0.3);
+            if (gamepad2.x) {
+                driveBase.drive(forward, strafe, turn);
+            }
+        }
+    }
+}
